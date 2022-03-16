16d1401c06a5719d73f45d5c97636382fefc4161
Nguyễn Vĩnh Hưng <nguyenvinhhung2307@gmail.com>
2022-03-16 21:03:20 +0700

    iamhung

--- js/js.js
+++ js/js.js
@@ -1,17 +1,20 @@
-function search(){
-    let search = document.querySelector('#search');
-    let content = Array.from(document.querySelectorAll('.content','content2'));
-    search.value = search.value.toLowerCase();
-    content.forEach(function(appear){
-        let text = appear.innerText;
-        if (text.indexOf(search.value)>-1)
-        appear.style.display="";
-        else appear.style.display="none";
-    })
-}
-//Hiện thị văn bản ra HTML
-document.getElementById('js').innerHTML = 'Wellcome to Javascript'
 
-let a = x,
-    b = y;
+//Hiện thị văn bản ra HTML
+//onmouseover = thay đổi màu thẻ khi di chuột vào
+myFunction = () => {
+    document.getElementById('js').innerHTML ='Wellcome to Javascript';
+}
+
+var random = Math.floor(Math.random()*100);
+
+    if (random < 50) {
+        console.log(document.getElementById('random').innerHTML ='success');
+    }else{
+        console.log(document.getElementById('random').innerHTML ='lost');
+    }
+
+
+document.getElementById('random').innerHTML = random;
+
+
 
