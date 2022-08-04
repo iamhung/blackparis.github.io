c81bc894bce1d9f4fd6ffb09d45392e701833b3b
Nguyễn Vĩnh Hưng <nguyenvinhhung2307@gmail.com>
2022-08-04 15:06:28 +0700

    ok

--- js/test.js
+++ js/test.js
@@ -1,37 +1,5 @@
-// var CORRECT_USER = 'iamhung';
-// var CORRECT_PASS = '123@';
 
-// var inputUsername = document.getElementById('username');
-// var inputPassword = document.getElementById('password');
-
-// var formLogin = document.getElementById('form-login');
-
-// if(formLogin.attachEvent) {
-//     formLogin.attachEvent ('submit',onFormSubmit);
-// }else {
-//     formLogin.addEvenListener ('submit',onFormSubmit);
-// }
-
-// function onFormSubmit (e) {
-//     var username = inputUserName.value;
-//     var password = inputPassword.value;
-
-//     if (username == CORRECT_USER && password == CORRECT_PASS) {
-//         alert('đăng nhập thành công');
-//     } else {
-//         alert('Đăng nhập thất bại');
-//     }
-// }
-// var a = 1;
-//     b = 2;
-//     c = a + b;
-
-//     if(c == 3) {
-//         window.location.href='/html/starbucks_home.html';
-//     }
-
-
-var dataprompt = prompt('Đây là web riêng tư !!!') ;
+var dataprompt = prompt('Xin lỗi !!! Đây là Web riêng tư !!!') ;
 // var delay = setTimeout(function(){
 //     window.location.href='/index.html';
 // },2000);
@@ -45,6 +13,8 @@ if(dataprompt == 'iamhung'){
         window.location.href='/index.html';
     },2500);
 }
+
+
     
 
     
